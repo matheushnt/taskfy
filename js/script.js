959e6f958c518cd409321e9c07644f8ec97c2d39
Matheus Henrique <matheushnt06@gmail.com>
2025-07-29 23:17:57 -0300

    Implementa funcionalidade de adicionar tarefa

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const botaoAdicionarTarefa = document.querySelector('.btn');
+const inputAdicionarTarefa = document.querySelector('.tarefa-input');
+const containerTarefas = document.querySelector('ul');
+const lixeiraIconeTarefas = document.querySelectorAll('.lixeira');
+const textoInfo = document.querySelector('.texto-info');
+
+const tarefas = [];
+
+const criarLiElement = descricao => {
+  const newLiElement = document.createElement('li');
+  newLiElement.classList.add('tarefa-item');
+  newLiElement.innerHTML = `
+    <div>
+      <input type="checkbox" name="tarefa-checkbox" id="tarefa-checkbox">
+      <label for="tarefa-checkbox" class="visually-hidden">Concluir tarefa</label>
+      <p class="tarefa-descricao">${descricao}</p>
+    </div>
+    <div class="lixeira">
+      <img src="./img/lixeira-icon.svg" alt="">
+    </div>
+    `;
+  return newLiElement;
+};
+
+const adicionarTarefa = () => {
+  const descricaoTarefa = inputAdicionarTarefa.value.trim();
+
+  if (descricaoTarefa) {
+    const newLiElement = criarLiElement(descricaoTarefa);
+    containerTarefas.appendChild(newLiElement);
+
+    textoInfo.classList.add('hidden');
+    containerTarefas.classList.add('ativo');
+
+    tarefas.push(descricaoTarefa);
+    inputAdicionarTarefa.value = '';
+  } else {
+    alert('Você precisa informar uma tarefa válida');
+    inputAdicionarTarefa.focus();
+  }
+};
+
+botaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
